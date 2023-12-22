7c146595da9689ea0dcfe1b0a66592a6af18adb0
Francois Lancelot <16709438+francoislanc@users.noreply.github.com>
2023-12-22 15:52:01 +0100

    Fix public path for prod build

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === "production" ? "/pyodide-vuejs-app-example/" : "/"
 })
